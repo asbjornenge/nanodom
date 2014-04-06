550127cf2331a5ef33d9f1bd0eadb027ebe0d229
Asbjørn Enge <asbjorn@hanafjedle.net>
2014-04-06 22:38:49 +0200

    Removed crush from build and did a new build for the other two

--- dist/0.0.2/nanodom.js
+++ dist/0.0.2/nanodom.js
@@ -1,13 +1,10 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+!function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var n;"undefined"!=typeof window?n=window:"undefined"!=typeof global?n=global:"undefined"!=typeof self&&(n=self),n.nanodom=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 function dom() {}
 dom.prototype             = new Array;
 dom.prototype.append      = function(element)   { element.map(function(e) { this[0].appendChild(e) }.bind(this)); return this }
 dom.prototype.remove      = function()          { this.map(function(e) {e.parentNode.removeChild(e)}); return this }
 dom.prototype.prepend     = function(element)   { element.map(function(e) { this[0].insertBefore(e, (this[0].hasChildNodes()) ? this[0].childNodes[0] : null) }.bind(this)); return this }
-dom.prototype.empty       = function(elements)  { this.map(function(e) { e.innerHTML = ""}); return this }
-dom.prototype.classes     = function()          { return ['add','remove','toggle','contain'].reduce(function(p,c) { p[c] = function() {
-    var a = arguments; this.map(function(e) { e.classList[c].apply(e.classList, a) }); return this
-}.bind(this); return p }.bind(this), {}) }
+dom.prototype.cmap        = function(fn)        { this.map(fn); return this }
 
 function domify(str) { var d = document.createElement('div'); d.innerHTML = str; return d.childNodes }
 
@@ -22,3 +19,5 @@ module.exports = function(selector) {
 }
 
 },{}]},{},[1])
+(1)
+});

d163c2484df4a0a271ff25d21a52f8109af21953
Asbjørn Enge <asbjorn@hanafjedle.net>
2014-04-06 22:38:24 +0200

    Removed crushed since it don't help gzipped

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp       = require('gulp')
 var browserify = require('gulp-browserify')
 var uglify     = require('gulp-uglify')
 var rename     = require('gulp-rename')
-var crush      = require('gulp-jscrush')
 var size       = require('gulp-size');
 var gzip       = require('gulp-gzip');
 var version    = require('./package.json').version
@@ -22,10 +21,6 @@ gulp.task('build', function() {
         .pipe(gulp.dest('./dist/'+version))
         .pipe(uglify())
         .pipe(rename('nanodom.min.js'))
-        .pipe(size({showFiles:true}))
-        .pipe(gulp.dest('./dist/'+version))
-        .pipe(crush())
-        .pipe(rename('nanodom.crushed.js'))
         .pipe(size({showFiles:true}))
         .pipe(gulp.dest('./dist/'+version))
 })
